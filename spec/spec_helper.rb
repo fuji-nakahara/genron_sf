aafe5a0a8704fa4ed39853a6e6d2440eedf5ab19
Fuji Nakahara <fujinakahara2032@gmail.com>
2020-12-26 14:45:18 +0900

    Set null logger in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@
 require 'bundler/setup'
 require 'genron_sf'
 
+GenronSF.config.logger = Logger.new(nil)
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
